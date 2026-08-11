963ec6382a848cca00a2e263b3d1f327a409929d
zakharkaverin1 <bina77790@gmail.com>
2026-08-12 01:20:56 +0200

    Updated main.tsx

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { MainPage } from '../pages/mainPage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-
+    <MainPage></MainPage>
   </StrictMode>
 )
