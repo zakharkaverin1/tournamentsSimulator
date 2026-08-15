581df0b4aa75e335e5ee639495c1670fc518da42
zakharkaverin1 <bina77790@gmail.com>
2026-08-15 15:50:21 +0200

    Added: switching between sections

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,22 @@
 import Button from "../Button/Button.tsx";
 import styles from "./Navbar.module.scss";
+import * as React from "react";
 
 
-const Navbar = () => {
+interface NavbarProps {
+    onChange: React.Dispatch<React.SetStateAction<string>>;
+}
 
+const Navbar = (props: NavbarProps) => {
+    const {onChange} = props;
     return (
         <nav className={styles.navbar}>
-            <Button children={"Tournaments"}/>
-            <Button children={"Teams"}/>
-            <Button children={"Players"}/>
-            <Button children={"Venues"}/>
+            <Button children={"Tournaments"} onClick={() => onChange("tournament")}/>
+            <Button children={"Teams"} onClick={() => onChange("team")}/>
+            <Button children={"Players"} onClick={() => onChange("player")}/>
+            <Button children={"Venues"} onClick={() => onChange("venue")}/>
         </nav>
+
     );
 }
 
