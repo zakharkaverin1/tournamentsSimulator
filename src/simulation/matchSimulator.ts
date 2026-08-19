c371841a55b5e2e564b9d607d85e18b90b8b072c
zakharkaverin1 <bina77790@gmail.com>
2026-08-19 19:41:28 +0200

    Make immutable

--- src/simulation/matchSimulator.ts
+++ src/simulation/matchSimulator.ts
@@ -4,11 +4,14 @@ import {expectedGoals, scoredGoals} from "../utils/functions.ts";
 export function simulateMatch(match: Match): Match {
     const [team1, team2] = [match.homeTeam, match.awayTeam];
     const firstTeamGoals = scoredGoals(expectedGoals(team1.level, team2.level));
-    const secondTeamGoals =  scoredGoals(expectedGoals(team2.level, team1.level));
-    match.homeScore = firstTeamGoals;
-    match.awayScore = secondTeamGoals;
-    match.winner = firstTeamGoals > secondTeamGoals ? match.homeTeam : match.awayTeam;
-    match.status = "FINISHED";
+    const secondTeamGoals = scoredGoals(expectedGoals(team2.level, team1.level));
     //правило выездного гола пока что. Ничья = победа гостей
-    return match;
+    const winner = firstTeamGoals > secondTeamGoals ? match.homeTeam : match.awayTeam;
+    return {
+        ...match,
+        homeScore: firstTeamGoals,
+        awayScore: secondTeamGoals,
+        winner: winner,
+        status: "FINISHED"
+    };
 }
